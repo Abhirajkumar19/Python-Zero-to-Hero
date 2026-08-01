bfa381f1a596a5b4a019d7cc4a354b245a39657f
Abhiraj Kumar <abhiraj19032007@gmail.com>
2026-08-01 08:52:22 +0530

    Add files via upload

---
+++ Chapter_04/01_list.py
@@ -0,0 +1,7 @@
+friends = ["apple", "orange", 5,345.06,"Aakash","Rohan"]
+
+print(friends[5])
+friends[5] = "Grapes" # unlike strings lists are mutabke
+
+print(friends[5])
+print(friends[1:5])
---
+++ Chapter_04/02_list_method.py
@@ -0,0 +1,8 @@
+friends = ["apple", "orange", 5,345.06,"Aakash","Rohan"]
+print (friends)
+friends.append("Harry")
+print(friends)
+
+l1 = [1, 34,25,5,36,48]
+l1.sort()
+print(11)
