cb70ed56cb03efe20708530782fe20462ff7404d
Abhiraj Kumar <abhiraj19032007@gmail.com>
2026-08-02 11:15:35 +0530

    Add files via upload

---
+++ Chapter_05_ps/Problem1.py
@@ -0,0 +1,8 @@
+words = {
+    "madad": "Hello",
+    "Kursi": "Chair",
+    "Billi": "Cat"
+}
+
+word = input("Enter the word you want meaning of: ")
+print(words[word])
---
+++ Chapter_05_ps/Problem8.py
@@ -0,0 +1,1 @@
+# Nothing Will happen. the value can be same
