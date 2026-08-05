c5138ef6658aebf72975b18e7eface6d8fcfa2cc
Abhiraj Kumar <abhiraj19032007@gmail.com>
2026-08-05 09:37:19 +0530

    Add files via upload

---
+++ Chapter_06_ps/Problem_5.py
@@ -0,0 +1,9 @@
+i = ["Abhi", "Aman", "Atul", "Rohan"]
+
+name = input("enter your name: ")
+
+if(name in i):
+    print("Your name is in the list")
+
+else:
+    print("Your name is not in the list")
---
+++ Chapter_06_ps/Problem_7.py
@@ -0,0 +1,7 @@
+Post = input("Enter the post: ")
+
+if("harry" in Post.lower()):
+    print("This post is talking about harry")
+
+else:
+    print("This post is not talking about harry" )
